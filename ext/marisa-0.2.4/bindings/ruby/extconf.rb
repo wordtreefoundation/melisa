edbc380adfc251a46af754490169317852bda322
Duane Johnson <duane.johnson@gmail.com>
2014-09-26 12:20:58 -0600

    Get extconf.rb working

--- ext/marisa-0.2.4/bindings/ruby/extconf.rb
+++ ext/marisa-0.2.4/bindings/ruby/extconf.rb
@@ -1,11 +1,32 @@
 require "mkmf"
 require "fileutils"
 
-FileUtils.cd(File.join(File.dirname(__FILE__), '..', '..')) do
-  system "./configure"
-  system "make"
+def sys(cmd)
+  puts "  -- #{cmd}"
+  unless ret = xsystem(cmd)
+    raise "#{cmd} failed, please report issue on https://github.com/wordtreefoundation/melisa"
+  end
+  ret
 end
 
-have_library("marisa")
+if `which make`.strip.empty?
+  STDERR.puts "\n\n"
+  STDERR.puts "***************************************************************************************"
+  STDERR.puts "*************** make required (apt-get install make build-essential) =( ***************"
+  STDERR.puts "***************************************************************************************"
+  exit(1)
+end
+
+MARISA_ROOT = File.join(File.dirname(__FILE__), '..', '..')
+PREFIX = File.expand_path('dest', MARISA_ROOT)
+
+FileUtils.cd(MARISA_ROOT) do
+  sys "./configure --prefix='#{PREFIX}'"
+  sys "make install"
+end
+
+$CFLAGS   << " -I#{File.join(PREFIX, 'include')}"
+$CPPFLAGS << " -I#{File.join(PREFIX, 'include')}"
+$LDFLAGS  << " -L#{File.join(PREFIX, 'lib')} -lmarisa"
 
 create_makefile("marisa")
