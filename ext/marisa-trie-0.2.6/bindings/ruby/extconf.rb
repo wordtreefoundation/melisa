0231dd73180cad1df09876642c30c00efac3945e
Ben D'Angelo <ben@bendangelo.me>
2024-09-18 01:47:59 -0400

    Fixes build

--- ext/marisa-trie-0.2.6/bindings/ruby/extconf.rb
+++ ext/marisa-trie-0.2.6/bindings/ruby/extconf.rb
@@ -1,5 +1,13 @@
 require "mkmf"
 
+PREFIX = File.expand_path(File.join(File.dirname(__FILE__), "..", "..", "..", "pkg"))
+include_path = File.join(PREFIX, 'include')
+
+flags = " -I#{include_path}"
+$CFLAGS   << flags
+$CXXFLAGS << flags
+$CPPFLAGS << flags
+
 have_library("marisa")
 
 create_makefile("marisa")
